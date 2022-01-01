d8e73bf8f104fff0dd91a7ed6515696b9c6b6e45
omarnassif98 <omar.nassif98@gmail.com>
2022-01-01 18:38:44 +0200

    Restructured europe map's svg and metadata. Set up the initialization process. Created helper function for resource requesting.

--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -1,18 +1,5 @@
-function ResourceRequest(url, method = 'GET', data = {}){
-    let contentHeader = (Object.keys(data).length > 0) ? 'application/json':'text/plain';
-    return new Promise ((resolve) => {
-        const req = new XMLHttpRequest();
-        req.open(method, url);
-        req.setRequestHeader('Content-type',contentHeader)
-        req.send(JSON.stringify(data));
-        req.onreadystatechange = function(){
-            if(req.readyState === XMLHttpRequest.DONE){
-                if(req.status == 200){
-                    resolve(req.response);
-                }else{
-                    resolve(req.status);
-                }
-            }
-        }
-    });
+function get_text_resource(url){
+    return new Promise((resolve) => {
+        fetch(url).then(res => res.text().then(txt => resolve(txt)))
+    })
 }
