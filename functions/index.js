365df6f23787a4dde8bba4a7ecd6dfa9961060cb
omarnassif98 <omar.nassif98@gmail.com>
2022-01-23 01:02:38 -0500

    Game Creation Complete

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,34 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const { error } = require("firebase-functions/logger");
+const { ref } = require("firebase-functions/v1/database");
 admin.initializeApp();
 exports.saveUserProfile = functions.https.onCall((data, context) => {    
     if(!context.auth){
-        rej('Not authenticated')
+        return {text:'Not logged in', code:'not-authenticated'}
     }
-    console.log(`request from user: ${context.auth.uid}`);
-    console.log(`data RAW: {${data.display_name}, ${data.beginner}}`);
+
     if(/^[a-zA-Z0-9 ]+$/.test(data.display_name) && !/^\s+$/.test(data.display_name)){
         console.log(`name ${data.display_name} is ok`);
-        return admin.firestore().collection('user_profiles').doc(context.auth.uid).set({ display_name: data.display_name, beginner: data.beginner}).then(() => {return {text:data.display_name, code:'good'}}).catch(() => {return {text:'Display name is ok but write failed', code:'firestore-write'}});
+        return admin.firestore().collection('users').doc(context.auth.uid).set({ display_name: data.display_name, beginner: data.beginner}).then(() => {return {text:data.display_name, code:'good'}}).catch(() => {return {text:'Display name is ok but write failed', code:'firestore-write'}});
     }else{
         console.log(`name ${data.display_name} fails`);
         return {text:'display name has forbidden characters', code:'forbidden-characters'};
     }
 });
+
+exports.createGameListing = functions.https.onCall((data, context) => {
+    if(!context.auth){
+        return {text:'Not logged in', code:'not-authenticated'}
+    }
+
+    if(/^[a-zA-Z0-9 ]+$/.test(data.game_name) && !/^\s+$/.test(data.game_name)){
+        let listing = {name : data.game_name, password : data.password, host : context.auth.uid, players : {FRA : null, ENG : null, TUR : null, RUS : null, AUS : null, GER : null}};
+        listing.players[data.nation] = context.auth.uid;
+        return admin.database().ref('pre_game_listings').push(listing).then(ref => {
+            return admin.database().ref(`user_games/${context.auth.uid}/${ref.key}`).set(data.game_name).then(() => {return {text:ref.key, code:'good'}}).catch(() => {return {text:ref.key, code:'user-game-error'}});
+        }).catch(() => {return {text:data.game_name, code:'game-listing-error'}});
+    }else{
+        return {text:'Lobby name has forbidden characters', code:'forbidden-characters'};
+    }
+})
--- public/scripts/business_logic/website/Browse.js
+++ public/scripts/business_logic/website/Browse.js
@@ -1,5 +1,25 @@
 function createGameLobby(){
-    if(document.getElementById('game_name_input').value == ''){
-        primeError(document.getElementById('game_create_form'), 'game_name', 'Innapropriate naming convention')
+    if(document.getElementById('name_input').value == ''){
+        primeError(document.getElementById('game_create_form'), 'name', 'Innapropriate naming convention');
+        return;
     }
+
+    if(document.getElementById('private_game_checkbox').checked && document.getElementById('password_input').value == ''){
+        primeError(document.getElementById('game_create_form'), 'password', 'Cannot be empty');
+        return;
+    }
+    let form = document.getElementById('game_create_form');
+    let lobby_info = {
+        game_name : form.querySelector('#name_input').value,
+        password : (form.querySelector('#private_game_checkbox').checked)?form.querySelector('#password_input').value:false,
+        nation : form.querySelector('#nation_select').getAttribute('val')
+    }
+
+    
+    let create_function = firebase.functions().httpsCallable('createGameListing');
+    create_function(lobby_info).then(res => {
+        console.log(res);
+    }).catch(res => console.error(res));
+
+
 }
